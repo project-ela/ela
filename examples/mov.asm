9760fcfe9cf70770f2c132c89a5d8052183e78c9
arata <arata04962@gmail.com>
2020-06-21 14:44:31 +0900

    :sparkles: implement JMP rm32 r32

--- examples/mov.asm
+++ examples/mov.asm
@@ -2,3 +2,5 @@ BITS 32
 org 0x7C00
 mov eax, 42
 mov ecx, 16
+mov eax, ecx
+mov ebx, eax
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,13 +1,29 @@
 use crate::emulator::Emulator;
 
 pub enum Opcode {
+    // 89 /r
+    MovRm32R32(usize, usize),
+    // B8+ rd id
     MovR32Imm32(usize, u32),
+    // EB cb
     ShortJump(usize),
+}
+
+#[derive(Debug)]
+pub struct ModRM {
+    pub modval: u8,
+    pub reg: u8,
+    pub rm: u8,
 }
 
 impl Emulator {
     pub fn decode(&mut self) -> Opcode {
         match self.get_code8(0) {
+            0x89 => {
+                self.eip += 1;
+                let modrm = self.parse_modrm();
+                Opcode::MovRm32R32(modrm.reg as usize, modrm.rm as usize)
+            }
             0xB8..=0xBF => {
                 let reg = self.get_code8(0) - 0xB8;
                 let value = self.get_code32(1);
@@ -16,6 +32,7 @@ impl Emulator {
             }
             0xEB => {
                 let diff = self.get_sign_code8(1);
+                // add 2 because of 'cb'
                 Opcode::ShortJump((diff + 2) as usize)
             }
             o => panic!("Not implemented: {:X}", o)
@@ -24,8 +41,22 @@ impl Emulator {
 
     pub fn exec(&mut self, opcode: Opcode) {
         match opcode {
+            Opcode::MovRm32R32(reg_from, reg_to) => self.registers[reg_to] = self.registers[reg_from],
             Opcode::MovR32Imm32(reg, value) =>self.registers[reg] = value,
             Opcode::ShortJump(diff) => self.eip = self.eip.wrapping_add(diff),
+        }
+    }
+
+    fn parse_modrm(&mut self) -> ModRM {
+        let code = self.get_code8(0);
+        let modval = (code & 0b11000000) >> 6;
+        let reg = (code & 0b00111000) >> 3;
+        let rm = code & 0b00000111;
+        self.eip += 1;
+        ModRM {
+            modval,
+            reg,
+            rm,
         }
     }
 }
