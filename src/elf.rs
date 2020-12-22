ecc8fa68a3e1f6c896bbb3df9d2f9e8455095a5a
arata <arata04962@gmail.com>
2020-12-22 19:00:14 +0900

    :wastebasket::construction: delete some functions

--- src/elf.rs
+++ src/elf.rs
@@ -1,11 +1,7 @@
-use std::mem::size_of;
-
 use segment::ElfProgramHeader;
-use symbol::Binding;
 
 use crate::header::ElfHeader;
 use crate::section::{ElfSectionHeader, Section};
-use crate::symbol::ElfSymbol;
 use crate::*;
 
 pub struct Elf {
@@ -25,131 +21,16 @@ impl Default for Elf {
 }
 
 impl Elf {
-    pub fn new() -> Self {
-        let mut elf = Self::default();
-
-        elf.add_null_section();
-        elf.add_shstrtab();
-        elf.add_symtab();
-        elf.add_null_symbol();
-
-        elf
-    }
-
     pub fn add_section(&mut self, name: &str, header: ElfSectionHeader, data: Vec<u8>) {
-        let mut header = header;
-
-        let strtab = self.find_section_mut(".shstrtab").unwrap();
-        header.name = strtab.data.len() as u32;
-        strtab.data.extend(name.as_bytes());
-        strtab.data.push(0);
-
         self.sections.push(Section {
             name: name.to_string(),
             header,
             data,
         });
-        self.header.section_header_num += 1;
-    }
-
-    pub fn find_section_mut(&mut self, name: &str) -> Option<&mut Section> {
-        self.sections
-            .iter_mut()
-            .filter(|section| section.name == name)
-            .next()
-    }
-
-    pub fn find_section_index(&self, name: &str) -> Option<usize> {
-        self.sections
-            .iter()
-            .enumerate()
-            .filter(|(_, section)| section.name == name)
-            .next()
-            .map(|(i, _)| i)
     }
 
     pub fn add_segment(&mut self, header: ElfProgramHeader) {
         self.segments.push(header);
-        self.header.program_header_num += 1;
-    }
-
-    pub fn add_symbol(&mut self, name: &str, symbol: ElfSymbol) {
-        let mut symbol = symbol;
-
-        let strtab = self.find_section_mut(".strtab").unwrap();
-        symbol.name = strtab.data.len() as u32;
-        strtab.data.extend(name.as_bytes());
-        strtab.data.push(0);
-
-        let symtab = self.find_section_mut(".symtab").unwrap();
-        symbol.write_to(&mut symtab.data);
-        if symbol.get_binding() == Some(Binding::Local) {
-            symtab.header.set_info(symtab.header.info + 1);
-        }
-    }
-
-    pub fn update_elf_header(&mut self) {
-        let mut data_length = 0;
-        data_length += size_of::<ElfHeader>();
-
-        self.header.program_header_offset = data_length as ElfOff;
-        data_length += self.header.program_header_size as usize * self.segments.len();
-
-        // skip null section
-        for section in self.sections.as_mut_slice().into_iter().skip(1) {
-            section.header.offset = data_length as ElfOff;
-            section.header.size = section.data.len() as ElfXword;
-            data_length += section.data.len();
-        }
-        self.header.section_header_offset = data_length as ElfOff;
-    }
-
-    fn add_symtab(&mut self) {
-        let mut symtab_hdr = ElfSectionHeader::default();
-        symtab_hdr.set_type(section::Type::Symtab);
-        symtab_hdr.set_link(self.sections.len() as u32 + 1);
-        symtab_hdr.set_entry_size(symbol::SYM_ENTRY_SIZE_64);
-        symtab_hdr.set_align(8);
-        self.add_section(".symtab", symtab_hdr, Vec::new());
-
-        let mut strtab_hdr = ElfSectionHeader::default();
-        strtab_hdr.set_type(section::Type::Strtab);
-        strtab_hdr.set_align(1);
-        self.add_section(".strtab", strtab_hdr, Vec::new());
-    }
-
-    fn add_shstrtab(&mut self) {
-        let mut shstrtab_hdr = section::ElfSectionHeader::default();
-        shstrtab_hdr.set_type(section::Type::Strtab);
-        shstrtab_hdr.set_align(1);
-
-        let mut data = Vec::new();
-        data.push(0);
-        shstrtab_hdr.name = data.len() as u32;
-        data.extend(b".shstrtab\0");
-
-        self.sections.push(Section {
-            name: ".shstrtab".to_string(),
-            header: shstrtab_hdr,
-            data,
-        });
-
-        self.header.section_header_num += 1;
-        self.header.string_table_index = self.sections.len() as ElfHalf - 1;
-    }
-
-    fn add_null_symbol(&mut self) {
-        self.add_symbol("", ElfSymbol::default());
-    }
-
-    fn add_null_section(&mut self) {
-        let header = ElfSectionHeader::default();
-        self.sections.push(Section {
-            name: "\0".to_string(),
-            header,
-            data: Vec::new(),
-        });
-        self.header.section_header_num += 1;
     }
 
     pub fn to_bytes(&self) -> Vec<u8> {
