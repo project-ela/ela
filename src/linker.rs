58d58e149e5bb81c6a9d986e905226407a8a419e
arata <arata04962@gmail.com>
2020-12-31 18:02:38 +0900

    :construction: add Linker

--- src/linker.rs
+++ src/linker.rs
@@ -1,4 +1,35 @@
+use std::fs;
+
+use elfen::elf::Elf;
+
 pub fn link(input_files: Vec<String>, output_file: String) -> Result<(), String> {
-    println!("link {:?} to {:?}", input_files, output_file);
+    let input_elfs = input_files
+        .into_iter()
+        .map(|path| Elf::read_from_file(&path))
+        .collect();
+    let linker = Linker::new(input_elfs);
+    let output_elf = linker.link()?;
+
+    let elf_bytes = output_elf.to_bytes();
+    fs::write(output_file, elf_bytes).unwrap();
+
     Ok(())
+}
+
+struct Linker {
+    input_elfs: Vec<Elf>,
+    output_elf: Elf,
+}
+
+impl Linker {
+    fn new(input_elfs: Vec<Elf>) -> Self {
+        Self {
+            input_elfs,
+            output_elf: Elf::default(),
+        }
+    }
+
+    fn link(mut self) -> Result<Elf, String> {
+        Ok(self.output_elf)
+    }
 }
