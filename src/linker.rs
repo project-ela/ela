5a5f4e4d46bcf41e3acaf6e61f5ecd6ab5ab21fd
arata <arata04962@gmail.com>
2020-12-31 21:26:14 +0900

    :sparkles: load relas

--- src/linker.rs
+++ src/linker.rs
@@ -2,6 +2,8 @@ use std::{collections::HashMap, fs};
 
 use elfen::{
     elf::Elf,
+    rel::Rela,
+    section,
     strtab::Strtab,
     symbol::{self, Symbol},
 };
@@ -31,6 +33,7 @@ struct Linker {
     output_elf: Elf,
 
     global_symbols: HashMap<String, SymbolSignature>,
+    relas: Vec<RelaSignature>,
 }
 
 #[derive(Debug)]
@@ -39,19 +42,53 @@ struct SymbolSignature {
     symbol: Symbol,
 }
 
+#[derive(Debug)]
+struct RelaSignature {
+    symbol_name: String,
+    rela: Rela,
+}
+
 impl Linker {
     fn new(input_elfs: Vec<Elf>) -> Self {
         Self {
             input_elfs,
             output_elf: Elf::default(),
             global_symbols: HashMap::new(),
+            relas: Vec::new(),
         }
     }
 
     fn link(mut self) -> Result<Elf, String> {
+        self.load_relas();
         self.load_symbols();
 
         Ok(self.output_elf)
+    }
+
+    fn load_relas(&mut self) {
+        for elf in self.input_elfs.iter_mut() {
+            let relas: Vec<Rela> = elf
+                .sections
+                .iter_mut()
+                .filter(|section| section.header.section_type == section::Type::Rela as u32)
+                .flat_map(|section| {
+                    std::mem::replace(section.data.as_rela_mut().unwrap(), Vec::new())
+                })
+                .collect();
+
+            let symtab_section = elf.get_section(".symtab").unwrap();
+            let symbols = symtab_section.data.as_symbols().unwrap();
+
+            let strtab_section = elf.get_section(".strtab").unwrap();
+            let strtab = strtab_section.data.as_strtab().unwrap();
+
+            for rela in relas {
+                let symbol = symbols.get(rela.get_symbol() as usize).unwrap();
+                let symbol_name = strtab.get(symbol.name as usize);
+
+                self.relas.push(RelaSignature { symbol_name, rela });
+            }
+        }
     }
 
     fn load_symbols(&mut self) {
