12269a8f5951cb0c61f64304658d2bca07b64345
arata <arata04962@gmail.com>
2020-12-31 17:49:23 +0900

    :sparkles: process arguments

---
+++ src/linker.rs
@@ -0,0 +1,4 @@
+pub fn link(input_files: Vec<String>, output_file: String) -> Result<(), String> {
+    println!("link {:?} to {:?}", input_files, output_file);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
+use herja::linker;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        println!("usage: herja <input_file>... <output_file>");
+        std::process::exit(0);
+    }
+
+    let args_num = args.len() - 1;
+    let input_files = args[1..args_num].to_vec();
+    let output_file = args[args_num].clone();
+
+    if let Err(err) = linker::link(input_files, output_file) {
+        println!("Failed to link: {}", err);
+        std::process::exit(1);
+    }
 }
