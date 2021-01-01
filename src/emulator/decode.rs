9d5ff139f994649d181e48e080158d7bb9f709ce
arata <arata04962@gmail.com>
2021-01-01 19:16:20 +0900

    :sparkles: load a elf and add segments

--- src/emulator/decode.rs
+++ src/emulator/decode.rs
@@ -5,10 +5,16 @@ use crate::emulator::Emulator;
 impl Emulator {
     pub fn decode(&mut self) -> Result<Instruction, String> {
         let rip = self.cpu.get_rip() as usize;
-        let code = self.mmu.get_memory64(rip).to_le_bytes().to_vec();
-        println!("Fetched: {:016X}", self.mmu.get_memory64(rip));
+        let mut bytes = Vec::new();
+        for i in 0..8 {
+            match self.mmu.get_memory8(rip + i) {
+                Ok(value) => bytes.push(value),
+                Err(_) => break,
+            }
+        }
+        println!("Fetched: {:X?}", bytes);
 
-        let mut decoder = decode::Decoder::new(&code);
+        let mut decoder = decode::Decoder::new(&bytes);
         let inst = decoder.decode_inst();
         let inst_len = decoder.pos();
         self.cpu.set_rip((rip + inst_len) as u64);
