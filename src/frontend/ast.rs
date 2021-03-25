c118a792b74c9fca1ad7205e037b6251b6206e40
arata <arata04962@gmail.com>
2021-03-25 23:13:22 +0900

    :recycle: split ast module into some files

--- src/frontend/ast.rs
+++ src/frontend/ast.rs
@@ -1,9 +1,10 @@
-use crate::common::{
-    operator::{BinaryOperator, UnaryOperator},
-    pos::Pos,
-    symtab::NodeId,
-    types::Type,
-};
+mod stmt;
+pub use stmt::*;
+
+mod expr;
+pub use expr::*;
+
+use crate::common::{pos::Pos, symtab::NodeId, types::Type};
 
 #[derive(Debug)]
 pub struct Module {
@@ -64,115 +65,4 @@ pub struct Function {
 pub struct Parameter {
     pub name: String,
     pub typ: Type,
-}
-
-#[derive(Debug)]
-pub struct Statement {
-    pub kind: StatementKind,
-    pub pos: Pos,
-    pub id: NodeId,
-}
-
-impl Statement {
-    pub fn new(kind: StatementKind, pos: Pos) -> Self {
-        Self {
-            kind,
-            pos,
-            id: NodeId::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-pub enum StatementKind {
-    Block {
-        stmts: Vec<Statement>,
-    },
-
-    Var {
-        name: String,
-        typ: Type,
-        value: Option<Box<Expression>>,
-    },
-    Val {
-        name: String,
-        typ: Type,
-        value: Option<Box<Expression>>,
-    },
-    Assign {
-        dst: Box<Expression>,
-        value: Box<Expression>,
-    },
-    Return {
-        value: Option<Box<Expression>>,
-    },
-    If {
-        cond: Box<Expression>,
-        then: Box<Statement>,
-        els: Option<Box<Statement>>,
-    },
-    While {
-        cond: Box<Expression>,
-        body: Box<Statement>,
-    },
-    // use this if return type is void
-    Call {
-        name: String,
-        args: Vec<Expression>,
-    },
-}
-
-#[derive(Debug, Clone)]
-pub struct Expression {
-    pub kind: ExpressionKind,
-    pub pos: Pos,
-    pub id: NodeId,
-}
-
-impl Expression {
-    pub fn new(kind: ExpressionKind, pos: Pos) -> Self {
-        Self {
-            kind,
-            pos,
-            id: NodeId::new(),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-pub enum ExpressionKind {
-    Char {
-        value: char,
-    },
-    Integer {
-        value: i32,
-    },
-    String {
-        value: String,
-    },
-    Bool {
-        value: bool,
-    },
-    Ident {
-        name: String,
-    },
-
-    UnaryOp {
-        op: UnaryOperator,
-        expr: Box<Expression>,
-    },
-    BinaryOp {
-        op: BinaryOperator,
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
-    },
-    // use this if return type isn't void
-    Call {
-        name: String,
-        args: Vec<Expression>,
-    },
-    Index {
-        lhs: Box<Expression>,
-        index: Box<Expression>,
-    },
 }
