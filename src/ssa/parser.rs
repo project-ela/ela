752a67bc2dd0317ddb8d0b575fc510477baa32f9
arata <arata04962@gmail.com>
2021-06-08 16:16:51 +0900

    :sparkles: parser: implement label parser and br instruction

--- src/ssa/parser.rs
+++ src/ssa/parser.rs
@@ -7,20 +7,23 @@ pub struct Module {
 pub struct Function {
     pub name: String,
     pub typ: String,
-    pub body: Vec<Block>,
+    pub body: Vec<Instruction>,
 }
 
 #[derive(Debug)]
-pub struct Block {
-    pub name: String,
-    pub inst: Vec<Instruction>,
-}
-
-#[derive(Debug)]
-pub struct Instruction {
-    pub dst: Option<Register>,
-    pub op: String,
-    pub src: Vec<Value>,
+pub enum Instruction {
+    O {
+        op: String,
+        src: Vec<Value>,
+    },
+    OD {
+        dst: Register,
+        op: String,
+        src: Vec<Value>,
+    },
+    L {
+        name: String,
+    },
 }
 
 #[derive(Debug)]
@@ -33,6 +36,7 @@ pub struct Value {
 pub enum ValueKind {
     Register(Register),
     Const(usize),
+    Label(String),
 }
 
 #[derive(Debug)]
@@ -43,6 +47,7 @@ pub struct Register {
 #[derive(Debug, Default)]
 struct Context {
     registers: HashMap<usize, ssa::Value>,
+    blocks: HashMap<String, ssa::BlockId>,
 }
 
 use std::collections::HashMap;
@@ -70,45 +75,54 @@ fn trans_func(f: Function, sm: &ssa::Module) -> ssa::Function {
     let mut fb = ssa::FunctionBuilder::new(&mut sf);
 
     let mut ctx = Context::default();
-    for block in f.body {
-        trans_block(block, &mut ctx, &mut fb);
+
+    for inst in &f.body {
+        if let Instruction::L { name } = inst {
+            ctx.blocks.insert(name.clone(), fb.new_block());
+        }
+    }
+
+    for inst in f.body {
+        trans_inst(inst, &mut ctx, &mut fb);
     }
 
     sf
 }
 
-fn trans_block(b: Block, ctx: &mut Context, fb: &mut ssa::FunctionBuilder) {
-    let block = fb.new_block();
-    fb.set_block(block);
-    for i in b.inst {
-        trans_inst(i, ctx, fb);
-    }
-}
-
 fn trans_inst(i: Instruction, ctx: &mut Context, fb: &mut ssa::FunctionBuilder) {
-    macro_rules! binop {
-        ($($op: ident),*) => {
-            match i.op.as_str() {
-                $(stringify!($op) => {
-                    let id = i.dst.unwrap().id;
-                    let lhs = trans_value(&i.src[0], ctx);
-                    let rhs = trans_value(&i.src[1], ctx);
-                    ctx.registers.insert(id, fb.$op(lhs, rhs));
-                    return;
-                }),*
-                _ => {}
+    match i {
+        Instruction::O { op, src } => match op.as_str() {
+            "ret" => {
+                let v = trans_value(&src[0], ctx);
+                fb.ret(v)
             }
-        };
-    }
+            "br" => {
+                let dst = trans_label(&src[0]);
+                fb.br(*ctx.blocks.get(dst).unwrap())
+            }
+            _ => unimplemented!(),
+        },
+        Instruction::OD { dst, op, src } => {
+            let lhs = trans_value(&src[0], ctx);
+            let rhs = trans_value(&src[1], ctx);
 
-    binop!(add, sub, mul, div, rem, shl, shr, and, or, xor, eq, neq, gt, gte, lt, lte);
+            macro_rules! binop {
+                ($($op: ident),*) => {
+                    match op.as_str() {
+                        $(stringify!($op) => {
+                            ctx.registers.insert(dst.id, fb.$op(lhs, rhs));
+                        }),*
+                        _ => {}
+                    }
+                };
+            }
 
-    match i.op.as_str() {
-        "ret" => {
-            let v = trans_value(&i.src[0], ctx);
-            fb.ret(v)
+            binop!(add, sub, mul, div, rem, shl, shr, and, or, xor, eq, neq, gt, gte, lt, lte);
         }
-        _ => unimplemented!(),
+        Instruction::L { name } => {
+            let block = ctx.blocks.get(&name).unwrap();
+            fb.set_block(*block);
+        }
     }
 }
 
@@ -116,6 +130,14 @@ fn trans_value(v: &Value, ctx: &mut Context) -> ssa::Value {
     match v.kind {
         ValueKind::Const(r#const) => ssa::Value::new_i32(r#const as i32),
         ValueKind::Register(Register { id }) => *ctx.registers.get(&id).unwrap(),
+        _ => panic!(),
+    }
+}
+
+fn trans_label(v: &Value) -> &String {
+    match v.kind {
+        ValueKind::Label(ref l) => l,
+        _ => panic!(),
     }
 }
 
@@ -139,7 +161,7 @@ peg::parser! {
             }
 
         rule function() -> Function
-            = "func" _ "@" name:ident() "(" _ ")" _ typ:ident() _"{" _ body:block()* _ "}"{
+            = "func" _ "@" name:ident() "(" _ ")" _ typ:ident() _"{" _ body:inst() ** _ _ "}"{
                 Function {
                     name,
                     typ,
@@ -147,41 +169,40 @@ peg::parser! {
                 }
             }
 
-        rule block() -> Block
-            = name:ident() ":" _ inst:inst()* {
-                Block {
-                    name,
-                    inst,
-                }
-            }
 
         rule inst() -> Instruction
-            =  op:ident() src:(_ src:value() _ {src})** "," {
-                Instruction {
-                    dst: None,
+            = name:ident() ":" { Instruction::L{name} }
+            / op:ident() src:(_ src:value() {src})** "," {
+                Instruction::O {
                     op,
                     src,
                 }
             }
-            / dst:reg() _ "=" _ op:ident() src:(_ src:value() _ {src}) ** "," {
-                Instruction {
-                    dst: Some(dst),
+            / dst:reg() _ "=" _ op:ident() src:(_ src:value() {src}) ** "," {
+                Instruction::OD{
+                    dst,
                     op,
                     src,
                 }
             }
 
         rule value() -> Value
-            = typ:ident() _ reg:reg() {
+            = "label" _ name:ident() {
                 Value {
-                    typ,
-                    kind: ValueKind::Register(reg),
+                    typ: "label".into(),
+                    kind: ValueKind::Label(name),
                 }
             }
             / typ:ident() _ r#const:number() {
                 Value {
                     typ,
                     kind: ValueKind::Const(r#const),
+                }
+            }
+            / typ:ident() _ reg:reg() {
+                Value {
+                    typ,
+                    kind: ValueKind::Register(reg),
                 }
             }
 
