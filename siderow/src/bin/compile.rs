bb5c6a526d47b92e54447af233d8c53fab398ece
Arata <arata04962@gmail.com>
2023-08-01 15:27:15 +0900

    feat(siderow: add a entrypoint for test

---
+++ siderow/src/bin/compile.rs
@@ -0,0 +1,27 @@
+use std::{env, fs, path::Path};
+
+use siderow::{arch::aarch64, ssa::parser};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: compile <filepath>");
+        return;
+    }
+
+    let program = load_file(Path::new(&args[1]));
+
+    let module = parser::parse(&program);
+    let mut assembly = aarch64::instsel::translate(module);
+    aarch64::regalloc::allocate(&mut assembly);
+    println!("{}", assembly.stringify());
+}
+
+fn load_file(path: &Path) -> String {
+    let input = fs::read_to_string(path).expect("cannot read file");
+    let mut input_lines = input.lines();
+
+    let _ = input_lines.next().unwrap();
+
+    input_lines.collect::<Vec<&str>>().join("\n")
+}
