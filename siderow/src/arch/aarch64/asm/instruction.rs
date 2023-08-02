91cb6d7689611a971ac162719acd20c132946e9c
Arata <arata04962@gmail.com>
2023-08-02 17:35:52 +0900

    feat(siderow): support ret without value

--- siderow/src/arch/aarch64/asm/instruction.rs
+++ siderow/src/arch/aarch64/asm/instruction.rs
@@ -5,23 +5,32 @@ use super::Printer;
 #[derive(Debug)]
 pub struct Instruction {
     pub mnemonic: Mnemonic,
+    pub operands: Vec<Operand>,
 }
 
 impl Instruction {
-    pub fn new(mnemonic: Mnemonic) -> Self {
-        Self { mnemonic }
+    pub fn new(mnemonic: Mnemonic, operands: Vec<Operand>) -> Self {
+        Self { mnemonic, operands }
     }
 }
 
 impl Printer for Instruction {
     fn print(&self, buf: &mut String) {
         self.mnemonic.print(buf);
+        write!(buf, " ");
+        for (i, operand) in self.operands.iter().enumerate() {
+            if i != 0 {
+                write!(buf, ", ");
+            }
+            operand.print(buf);
+        }
         writeln!(buf, "");
     }
 }
 
 #[derive(Debug)]
 pub enum Mnemonic {
+    B,
     Ret,
 }
 
@@ -30,8 +39,24 @@ impl Printer for Mnemonic {
         use self::Mnemonic::*;
 
         let s = match self {
+            B => "b",
             Ret => "ret",
         };
         write!(buf, "{}", s);
+    }
+}
+
+#[derive(Debug)]
+pub enum Operand {
+    Label(String),
+}
+
+impl Printer for Operand {
+    fn print(&self, buf: &mut String) {
+        use self::Operand::*;
+
+        match self {
+            Label(name) => write!(buf, "{}", name),
+        };
     }
 }
--- siderow/src/arch/aarch64/instsel/function.rs
+++ siderow/src/arch/aarch64/instsel/function.rs
@@ -10,17 +10,61 @@ impl<'a> FunctionTransrator<'a> {
         Self { module, function }
     }
 
-    pub fn translate(self) -> asm::Function {
+    pub fn translate(mut self) -> asm::Function {
         let mut asm_func = asm::Function::new(&self.function.name);
 
         for block_id in &self.function.block_order {
             asm_func.add_label(self.block_label(block_id));
+            let block = self.function.block(*block_id).unwrap();
+            self.trans_block(block, &mut asm_func);
         }
 
         asm_func.add_label(self.return_label());
-        asm_func.add_inst(asm::Instruction::new(asm::Mnemonic::Ret));
+        asm_func.add_inst(asm::Instruction::new(asm::Mnemonic::Ret, vec![]));
 
         asm_func
+    }
+
+    fn trans_block(&mut self, block: &ssa::Block, asm_func: &mut asm::Function) {
+        for inst_id in &block.instructions {
+            let ssa_inst = self.function.inst(*inst_id).unwrap();
+            let asm_inst = self.trans_inst(ssa_inst);
+            for inst in asm_inst {
+                asm_func.add_inst(inst);
+            }
+        }
+
+        let Some(term_id) = block.terminator else { return; };
+        let ssa_inst = self.function.inst(term_id).unwrap();
+        let asm_inst = self.trans_term(ssa_inst);
+        for inst in asm_inst {
+            asm_func.add_inst(inst);
+        }
+    }
+
+    fn trans_inst(&mut self, inst: &ssa::Instruction) -> Vec<asm::Instruction> {
+        vec![]
+    }
+
+    fn trans_term(&mut self, inst: &ssa::Instruction) -> Vec<asm::Instruction> {
+        use ssa::InstructionKind::*;
+
+        match &inst.kind {
+            Ret(val) => {
+                let mut inst = Vec::new();
+                match val {
+                    None => {}
+                    Some(val) => unimplemented!(),
+                }
+
+                inst.push(asm::Instruction::new(
+                    asm::Mnemonic::B,
+                    vec![asm::Operand::Label(self.return_label())],
+                ));
+                inst
+            }
+            _ => unimplemented!(),
+        }
     }
 
     fn block_label(&self, block_id: &ssa::BlockId) -> String {
